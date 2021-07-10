653c0568b491dfb4606a6593fdd2450f457a7196
nesto123 <fran.vojkovic@gmail.com>
2021-07-10 17:38:18 +0200

    comments removed

--- DP/src/app/App1.java
+++ DP/src/app/App1.java
@@ -26,13 +26,9 @@ public class App1 extends Process implements CamUser{
     }
     public synchronized void handleMsg(Msg m, int src, String tag){
         if (tag.equals("chat")) {
-            //System.out.println(m.getMessage());
             FileSystem files = new FileSystem(String.valueOf(myId));
-            //BufferedReader din = new BufferedReader(
-    			//	new InputStreamReader(System.in));
             String fileName=m.getTitle();
 			try {
-				//fileName = App1.getUserInput(din, "Enter new file name:");
 				if(files.createFile(fileName,m.getMessage()) ){
 	        		System.out.println("File " + fileName + " recived from " + src + ".");
                     System.out.println( "Enter command (create,send,list,delete):" );
@@ -50,18 +46,7 @@ public class App1 extends Process implements CamUser{
         return chatMsg;
     }
     public IntLinkedList getDest(BufferedReader din) throws Exception {
-//        System.out.println("Type in destination pids with -1 at end:");
-//        System.out.println("Only one pid for synch order:");
-//        IntLinkedList destIds = new IntLinkedList(); //dest for msg
-//        StringTokenizer st = new StringTokenizer(din.readLine());
-//        while (st.hasMoreTokens()) {
-//            int pid = Integer.parseInt(st.nextToken());
-//            if (pid == -1) break;
-//            else destIds.add(pid);
-//        }
-//        return destIds;
-//        
-        ////
+
         System.out.println("Type in destination: ");
         IntLinkedList destIds = new IntLinkedList(); //dest for msg
         StringTokenizer st = new StringTokenizer(din.readLine());
@@ -94,36 +79,24 @@ public class App1 extends Process implements CamUser{
         
         String[] args1 =  App1.getUserInput(din, "Enter arguments: ").split(" ");
         
-    	// Mo�da prebacit sve da se �ita sa ulaza, a ne sa komandne linije.
-        String baseName = args1[0];								//	Ime aplikacije
-        myId = Integer.parseInt(args1[1]);					//	Broj tog processa
-        int numProc = Integer.parseInt(args1[2]);				//	Ukupan broj procesa
+        myId = Integer.parseInt(args1[0]);					//	Broj tog processa
+        int numProc = Integer.parseInt(args1[1]);				//	Ukupan broj procesa
         Linker comm = null;      
         
         
-        if (args1[3].equals("simple"))							//	FIFO -- ne koristiri 
-            comm = new Linker(baseName, myId, numProc);
-        else if (args1[3].equals("causal"))						//	Kauzalni -- bolje
-            comm = new CausalLinker(baseName, myId, numProc);
-        else
-        	throw new Exception("Invalid comandline arguments!");
+        comm = new Linker("app", myId, numProc);
         
         App1 c = new App1(comm);
         Camera camera = new TermRecvCamera( comm, c, new DSTerm( comm ), numProc );
-
-        /*for (int i = 0; i < numProc; i++)
-            if (i != myId) 
-            	(new ListenerThread(i, c)).start();*/
 
         for (int i = 0; i < numProc; i++)
             if (i != myId) 
                 (new ListenerThread(i, camera)).start();
 
-        //g.makeTree();
         
         FileSystem files = new FileSystem(String.valueOf(myId));
         while (true) {
-            String chatMsg = App1.getUserInput(din, "Enter command (create,send,list,delete):");
+            String chatMsg = App1.getUserInput(din, "Enter command (create,send,list,delete,snapshot):");
             
             if (chatMsg.equals("quit")) 
         	{
